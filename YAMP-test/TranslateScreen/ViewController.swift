730e097dd6706dd613f067b943b800de9206d6a7
Serge Nanaev <serge-nanaev@yandex-team.ru>
2021-10-21 20:39:24 +0300

    Translate Screen init

---
+++ YAMP-test/TranslateScreen/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class TranslateViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func translateButtonDidTapped(_ sender: Any) { }
+}
