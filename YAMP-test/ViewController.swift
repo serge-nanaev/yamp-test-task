ef3132a3546944058ba0f1ce255c1b8260a2756e
Serge Nanaev <serge-nanaev@yandex-team.ru>
2021-10-21 19:50:08 +0300

    Default Xcode app

---
+++ YAMP-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YAMP-test
+//
+//  Created by Sergey Nanaev on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
